f263d2519f09ba9780aed4dc4df074b68e178c2b
YanRemes <83425187+YanRemes@users.noreply.github.com>
2021-12-06 10:56:05 -0800

    Update app_server.R

--- app_server.R
+++ app_server.R
@@ -3,6 +3,13 @@ library(ggplot2)
 library(dplyr)
 
 data <- read.csv('~/Desktop/info201/final project/Data/survey.csv')
+data <- data %>% filter(grepl('2014', data$Timestamp))
+data$Gender <- toupper(data$Gender)
+data$Gender[grep("\\bM\\b",data$Gender)] <- 'MALE'
+data$Gender[grep("\\bF\\b",data$Gender)] <- 'FEMALE'
+data$Gender[grep("\\bWOMAN\\b",data$Gender)] <- 'FEMALE'
+keepGender <- c("MALE", "FEMALE")
+data <- data[data$Gender %in% keepGender, ]
 
 server <- function(input, output) {
   
